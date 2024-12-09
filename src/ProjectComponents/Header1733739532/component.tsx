4de70fd7dc0a99191b5aaf0623c400294d08f46e
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-09 16:10:49 +0100

    feat: Edit Header1733739532
    Requested change: Increase the text size

--- src/ProjectComponents/Header1733739532/component.tsx
+++ src/ProjectComponents/Header1733739532/component.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
   return (
     <header className="bg-red-600 text-yellow-400 p-4 w-full h-full">
       <div className="container mx-auto flex justify-between items-center h-full">
-        <div className="text-5xl font-bold">Your next best favorite is here</div>
+        <div className="text-6xl font-bold">Your next best favorite is here</div>
       </div>
     </header>
   );
